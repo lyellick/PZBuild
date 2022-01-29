d973d85cd41f01a9457e7d725b68ac7c0c9db2da
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-01-28 23:04:37 -0600

    PZBuild.Common.Test Initial Create + Initial Sqlite provider test

---
+++ PZBuild/PZBuild.Common.Test/ContextTests/PZBuildContextTest.cs
@@ -0,0 +1,21 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PZBuild.Common.Context;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PZBuild.Common.Test.ContextTests
+{
+    public class PZBuildContextTest
+    {
+        [Fact]
+        public async Task PZBuildContext_UsingSqliteInMemoryProvider_EnsureCreatedAsync()
+        {
+            using SqliteConnection connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            DbContextOptions<PZBuildContext>? options = new DbContextOptionsBuilder<PZBuildContext>().UseSqlite(connection).Options;
+            using PZBuildContext context = new PZBuildContext(options);
+            Assert.True(await context.Database.EnsureCreatedAsync());
+        }
+    }
+}
