a52c780596e86425248f328bd39bf76edf0d90a9
Lincoln Hach Yellick <lincoln.yellick@gmail.com>
2022-02-01 21:14:55 -0600

    Added occupation and traits selection UX and on load UI sets

--- PZBuild/PZBuild.Web.Site/wwwroot/site/global.js
+++ PZBuild/PZBuild.Web.Site/wwwroot/site/global.js
@@ -1,1 +1,6 @@
-﻿
+﻿function message(message) { document.querySelector("#alert div span").innerText = message, document.getElementById("alert").classList.remove("close"), document.getElementById("alert").classList.add("open"), setTimeout(() => { document.getElementById("alert").classList.remove("open"), document.getElementById("alert").classList.add("close") }, 3500) }
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+});
